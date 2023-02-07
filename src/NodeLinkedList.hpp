0bf3c2875af7d170d4183df3645fc9cccdb84a6d
Lucas Inocencio <113804674+lucas-inocencio@users.noreply.github.com>
2023-02-07 00:15:40 -0300

    Update NodeLinkedList.hpp

--- src/NodeLinkedList.hpp
+++ src/NodeLinkedList.hpp
@@ -1,45 +1,36 @@
 class NodeLinkedList {
 
     public:
-        int data_int, capacity, allocation_flow;
-        bool reverse;
-        float data_float;
-        NodeLinkedList* next_node;
+        int id, capacity, allocation_flow;
+        bool isReverse;
+        NodeLinkedList *next;
 
         NodeLinkedList();
-        NodeLinkedList(int, float);
         NodeLinkedList(int, int);
         NodeLinkedList(int, int, bool);
 };
 
 NodeLinkedList::NodeLinkedList() {
 
-    this->data_int = 0;
-    this->capacity = 0;
-    this->allocation_flow = 0;
-    this->reverse = false;
-    this->next_node = nullptr;
+    id = 0;
+    capacity = 0;
+    allocation_flow = 0;
+    isReverse = false;
+    next = nullptr;
 }
 
-NodeLinkedList::NodeLinkedList(int new_data_int, float new_data_float) { // O(1)
+NodeLinkedList::NodeLinkedList(int new_id, int new_capacity) { // O(1)
     
-    this->data_int = new_data_int;
-    this->data_float = new_data_float;
-    this->next_node = nullptr;
+    id = new_id;
+    capacity = new_capacity;
+    allocation_flow = 0;
+    next = nullptr;
 }
 
-NodeLinkedList::NodeLinkedList(int new_data_int, int new_capacity) { // O(1)
+NodeLinkedList::NodeLinkedList(int new_id, int new_capacity, bool new_reverse) { // O(1)
     
-    this->data_int = new_data_int;
-    this->capacity = new_capacity;
-    this->allocation_flow = 0;
-    this->next_node = nullptr;
-}
-
-NodeLinkedList::NodeLinkedList(int new_data_int, int new_capacity, bool new_reverse) { // O(1)
-    
-    this->data_int = new_data_int;
-    this->capacity = new_capacity;
-    this->reverse = new_reverse;
-    this->next_node = nullptr;
+    id = new_id;
+    capacity = new_capacity;
+    isReverse = new_reverse;
+    next = nullptr;
 }
