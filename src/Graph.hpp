03cee5ea4e2241cc9af8cffe9f1743ec48b24468
Lucas Inocencio <113804674+lucas-inocencio@users.noreply.github.com>
2023-02-07 00:12:21 -0300

    Update Graph.hpp

--- src/Graph.hpp
+++ src/Graph.hpp
@@ -2,6 +2,17 @@
 #include "AdjacencyList.hpp"
 #include "BinaryHeap.hpp"
 
+unsigned highest_power2(unsigned x) {
+
+    x |= x >> 1;
+    x |= x >> 2;
+    x |= x >> 4;
+    x |= x >> 8;
+    x |= x >> 16;
+
+    return x ^ (x >> 1);
+}
+
 class Graph {
 
     public:
@@ -14,13 +25,13 @@ class Graph {
         Graph();
         void builder_adjacency_list(std::string, std::string);
         void breadth_first_search(int, int);
-        void get_path(int);
+        void find_path(int);
         void uniform_cost_search_array(int, int);
         void uniform_cost_search_heap(int, int);
         void find_mst_heap(int);
-        void builder_residual();
-        int get_bottleneck(int, int);
+        int find_bottleneck(int, int);
         int find_max_flow(int, int);
+        AdjacencyList* find_netflow(int, int, std::string);
 };
 
 Graph::Graph() {
@@ -33,11 +44,11 @@ void Graph::builder_adjacency_list(std::string file_name, std::string type) { //
 
     representation = new AdjacencyList(file_name, type);
     number_vertex = representation->number_vertex;
-    residual = new LinkedList[number_vertex]();
-    parent = new int[number_vertex]{};
-    reverse_path = new bool[number_vertex]{};
-    label = new bool[number_vertex]{};
     path = new LinkedList();
+    residual = new AdjacencyList(file_name, "residual");
+    parent = new int[number_vertex]{};
+    reverse_edge = new bool[number_vertex]{};
+    visited = new bool[number_vertex]{};
 }
 
 void Graph::breadth_first_search(int root, int delta) { // O(V+E)
@@ -48,41 +59,44 @@ void Graph::breadth_first_search(int root, int delta) { // O(V+E)
 
     for (int i = 0; i < number_vertex; i++) {
         parent[i] = -1;
-        label[i] = false;
-        reverse_path[i] = false;
+        visited[i] = false;
+        reverse_edge[i] = false;
     }
 
     discovered.push(root);
-    label[root-1] = true;
+    visited[root-1] = true;
     parent[root-1] = 0;
 
     while (discovered.empty() == false) {
 
         next_vertex = discovered.front();
         discovered.pop();
-        adjacent_vertex = residual[next_vertex-1].head;
+        adjacent_vertex = residual->linked_list_array[next_vertex-1].head;
 
-        for (int i = 1; i <= residual[next_vertex-1].size; i++) {
+        for (int i = 0; i < residual->linked_list_array[next_vertex-1].size; i++) {
             
-            if ((label[adjacent_vertex->data_int-1] == false) && (adjacent_vertex->capacity > delta)) {
-                label[adjacent_vertex->data_int-1] = true;
-                discovered.push(adjacent_vertex->data_int);
-                parent[adjacent_vertex->data_int-1] = next_vertex;
-                if (adjacent_vertex->reverse == true) {reverse_path[adjacent_vertex->data_int-1] = true;}
+            if ((visited[adjacent_vertex->id-1] == false) && (adjacent_vertex->capacity > delta)) {
+                visited[adjacent_vertex->id-1] = true;
+                discovered.push(adjacent_vertex->id);
+                parent[adjacent_vertex->id-1] = next_vertex;
+                if (adjacent_vertex->isReverse == true) {
+                    reverse_edge[adjacent_vertex->id-1] = true;
+                }
             }
 
-            adjacent_vertex = adjacent_vertex->next_node;
+            adjacent_vertex = adjacent_vertex->next;
         }
     }
+
 }
 
-void Graph::get_path(int target) {
+void Graph::find_path(int target) {
 
-    delete path;
-    path = new LinkedList();
+    path->remove_all();
     int temp = target;
     
     if (parent[target-1] == -1) {
+        "pass";
     } else {
         path->insert(temp, 0);
         while (parent[temp-1] != 0) {
@@ -245,141 +259,175 @@ void Graph::find_mst_heap(int source) {
     mst_file.close();
 }
 
-void Graph::builder_residual() {
+int Graph::find_bottleneck(int source, int target) {
 
-    NodeLinkedList *adjancent_vertex;
-    
-    for (int next_vertex = 1; next_vertex <= number_vertex; next_vertex++) {
-        
-        adjancent_vertex = representation->linked_list_array[next_vertex-1].head;
-        for (int i = 1; i <= representation->linked_list_array[next_vertex-1].size; i++) { // O(E)
-            residual[next_vertex-1].insert(adjancent_vertex->data_int, adjancent_vertex->capacity, false);
-            residual[adjancent_vertex->data_int-1].insert(next_vertex, 0, true);
-            adjancent_vertex = adjancent_vertex->next_node;
-        }
-    }
-}
-
-int Graph::get_bottleneck(int source, int target) {
-
-    if (parent[target-1] == -1) {
+    if (path->size == 0) {
         return 0;
     } else {
+
         int bottleneck = 1000000000;
         NodeLinkedList *walk_path, *walk_residual;
 
         walk_path = path->head;
-        int next_vertex = walk_path->data_int;
-        walk_path = walk_path->next_node;
-        int adjacent_vertex = walk_path->data_int;
+        int next_vertex;
+        int adjacent_vertex = walk_path->id;
+        
+        for (int i = 1; i < path->size; i++) {
 
-        walk_residual = residual[next_vertex-1].head;
-            
-        while (walk_residual->data_int != adjacent_vertex) {
-            walk_residual = walk_residual->next_node;
-        }
-        if (walk_residual->capacity < bottleneck) {
-            bottleneck = walk_residual->capacity;
-        }
-
-        for (int i = 2; i < path->size; i++) {
             next_vertex = adjacent_vertex;
-            walk_path = walk_path->next_node;
-            adjacent_vertex = walk_path->data_int;
-            walk_residual = residual[next_vertex-1].head;
-            
-            while (walk_residual->data_int != adjacent_vertex) {
-                walk_residual = walk_residual->next_node;
+            walk_path = walk_path->next;
+            adjacent_vertex = walk_path->id;
+
+            if (reverse_edge[adjacent_vertex-1] == true) {
+
+                walk_residual = residual->linked_list_array[adjacent_vertex-1].head;
+                while (walk_residual->id != next_vertex) {
+                    walk_residual = walk_residual->next;
+                }
+
+            } else {
+
+                walk_residual = residual->linked_list_array[next_vertex-1].head;
+                while (walk_residual->id != adjacent_vertex) {
+                    walk_residual = walk_residual->next;
+                }
             }
+            
             if (walk_residual->capacity < bottleneck) {
                 bottleneck = walk_residual->capacity;
             }
         }
 
+        
         return bottleneck;
     }
 }
 
-unsigned highestPowerof2(unsigned x) {
-
-    x |= x >> 1;
-    x |= x >> 2;
-    x |= x >> 4;
-    x |= x >> 8;
-    x |= x >> 16;
-
-    return x ^ (x >> 1);
-}
-
 int Graph::find_max_flow(int source, int target) {
 
-    builder_residual();
-    LinkedList *x;
-    NodeLinkedList *curr_path, *walk_residual_original, *walk_residual_reverse, *walk_graph_original;
+    NodeLinkedList *curr_path, *residual_original, *residual_reverse, *graph_original, *out_capacity;
     int bottleneck, next_vertex, adjacent_vertex;
     int max_flow = 0;
     int delta = 0;
 
+    out_capacity = representation->linked_list_array[source-1].head;
     for (int i = 0; i < representation->linked_list_array[source-1].size; i++) {
-        delta += walk_graph_original->capacity;
-        walk_graph_original = walk_graph_original->next_node;
+        delta += out_capacity->capacity;
+        out_capacity = out_capacity->next;
     }
 
-    delta = highestPowerof2(delta);
+    delta = highest_power2(delta);
     breadth_first_search(source, delta);
 
-    while (true) {
+    while (parent[target-1] > 0 || delta > 0) {
+        
+        curr_path = path->head;
+        bottleneck = find_bottleneck(source, target);
 
-        get_path(target);
-        if (path->head == nullptr) {
-            break;
-        } else {
-            curr_path = path->head;
-        }
-
-        bottleneck = get_bottleneck(source, target);
-        if (bottleneck == 0) {break;}
-
-        while (curr_path->next_node != nullptr) {
+        for (int i = 1; i < path->size; i++) {
             
-            next_vertex = curr_path->data_int;
-            curr_path = curr_path->next_node;
-            adjacent_vertex = curr_path->data_int;
-            walk_residual_original = residual[next_vertex-1].head;
-            walk_residual_reverse = residual[adjacent_vertex-1].head;
-            walk_graph_original = representation->linked_list_array[next_vertex-1].head;
+            next_vertex = curr_path->id;
+            curr_path = curr_path->next;
+            adjacent_vertex = curr_path->id;
+            residual_original = residual->linked_list_array[next_vertex-1].head;
+            residual_reverse = residual->linked_list_array[adjacent_vertex-1].head;
 
-            while (walk_residual_original->data_int != adjacent_vertex) {
-                walk_residual_original = walk_residual_original->next_node;
+            while (residual_original->id != adjacent_vertex) {
+                residual_original = residual_original->next;
             }
-            while (walk_residual_reverse->data_int != next_vertex) {
-                walk_residual_reverse = walk_residual_reverse->next_node;
-            }
-            while (walk_graph_original->data_int != adjacent_vertex) {
-                walk_graph_original = walk_graph_original->next_node;
+            while (residual_reverse->id != next_vertex) {
+                residual_reverse = residual_reverse->next;
             }
 
-            if (reverse_path[adjacent_vertex-1] == true) {
-                walk_residual_original->capacity += bottleneck;
-                walk_residual_reverse->capacity -= bottleneck;
-                walk_graph_original->allocation_flow -= bottleneck;
-                std::cout << "reversa: " << bottleneck << "\n";
-            } else if (reverse_path[adjacent_vertex-1] == false) {
-                walk_residual_original->capacity -= bottleneck;
-                walk_residual_reverse->capacity += bottleneck;
-                walk_graph_original->allocation_flow += bottleneck;
-                std::cout << "original: " << bottleneck << "\n";
-            }
+            residual_original->capacity -= bottleneck;
+            residual_reverse->capacity += bottleneck;
         }
+
         delta = delta/2;
         breadth_first_search(source, delta);
+        find_path(target);
     }
-    
-    walk_graph_original = representation->linked_list_array[source-1].head;
+
+    out_capacity = representation->linked_list_array[source-1].head;
+    residual_original = residual->linked_list_array[source-1].head;
     for (int i = 0; i < representation->linked_list_array[source-1].size; i++) {
-        max_flow += walk_graph_original->allocation_flow;
-        walk_graph_original = walk_graph_original->next_node;
+        max_flow += out_capacity->capacity - residual_original->capacity;
+        out_capacity = out_capacity->next;
+        residual_original = residual_original->next;
     }
 
     return max_flow;
+}
+
+AdjacencyList* Graph::find_netflow(int source, int target, std::string write) {
+
+    NodeLinkedList *curr_path, *residual_original, *residual_reverse, *graph_original, *out_capacity;
+    int bottleneck, next_vertex, adjacent_vertex;
+    int max_flow = 0;
+    int delta = 0;
+
+    out_capacity = representation->linked_list_array[source-1].head;
+    for (int i = 0; i < representation->linked_list_array[source-1].size; i++) {
+        delta += out_capacity->capacity;
+        out_capacity = out_capacity->next;
+    }
+
+    delta = highest_power2(delta);
+    breadth_first_search(source, delta);
+
+    while (parent[target-1] > 0 || delta > 0) {
+        
+        curr_path = path->head;
+        bottleneck = find_bottleneck(source, target);
+
+        for (int i = 1; i < path->size; i++) {
+            
+            next_vertex = curr_path->id;
+            curr_path = curr_path->next;
+            adjacent_vertex = curr_path->id;
+            residual_original = residual->linked_list_array[next_vertex-1].head;
+            residual_reverse = residual->linked_list_array[adjacent_vertex-1].head;
+            graph_original = representation->linked_list_array[next_vertex-1].head;
+
+            while (residual_original->id != adjacent_vertex) {
+                residual_original = residual_original->next;
+            }
+            while (residual_reverse->id != next_vertex) {
+                residual_reverse = residual_reverse->next;
+            }
+            while (graph_original->id != adjacent_vertex) {
+                graph_original = graph_original->next;
+            }
+
+            if (reverse_edge[adjacent_vertex-1] == true) {
+                residual_original->capacity += bottleneck;
+                residual_reverse->capacity -= bottleneck;
+                graph_original->allocation_flow -= bottleneck;
+            } else {
+                residual_original->capacity -= bottleneck;
+                residual_reverse->capacity += bottleneck;
+                graph_original->allocation_flow += bottleneck;
+            }
+        }
+
+        delta = delta/2;
+        breadth_first_search(source, delta);
+        find_path(target);
+    }
+
+    if (write == "w") {
+
+        std::ofstream outfile ("output_netflow.txt");
+        outfile << number_vertex << std::endl;
+        for (int i = 0; i < number_vertex; i++) {
+            graph_original = representation->linked_list_array[i].head;
+            for (int j = 0; j < representation->linked_list_array[i].size; j++) {
+                outfile << i+1 << " " << graph_original->id << " " << graph_original->allocation_flow << std::endl;
+                graph_original = graph_original->next;
+            }
+        }
+    }
+    
+
+    return representation;
 }
