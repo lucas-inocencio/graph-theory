4f5b7ab6bb6cd30b036694d74d37219ddfc6bdc2
Lucas Inocencio <lucas-inocencio@users.noreply.github.com>
2023-03-27 22:45:20 -0300

    DFS method.

--- src/GraphSearcher.hpp
+++ src/GraphSearcher.hpp
@@ -2,6 +2,7 @@
 #define GRAPH_SEARCHER_HPP
 
 #include <queue>
+#include <stack>
 
 #include "./Representations/AdjacencyList.hpp"
 
@@ -12,6 +13,7 @@ class GraphSearcher
 {
 public:
     void breadth_first_search(AdjacencyList *, int);
+    void depth_first_search(AdjacencyList *, int);
 };
 
 /**
@@ -40,6 +42,48 @@ void GraphSearcher::breadth_first_search(AdjacencyList *graph, int root)
     while (discovered.empty() == false)
     {
         next_vertex = discovered.front();
+        discovered.pop();
+        adjacent_vertex = graph->array[next_vertex - 1].head;
+        for (int i = 0; i < graph->array[next_vertex - 1].size; i++)
+        {
+            if (visited[adjacent_vertex->value - 1] == false)
+            {
+                visited[adjacent_vertex->value - 1] = true;
+                discovered.push(adjacent_vertex->value);
+                parent[adjacent_vertex->value - 1] = next_vertex;
+            }
+            adjacent_vertex = adjacent_vertex->next;
+        }
+    }
+}
+
+
+/**
+ * @brief Perform a depth-first search on the specified graph, starting from the given root vertex.
+ * @param graph Pointer to the adjacency list representing the graph.
+ * @param root The id (value) of the root vertex to start the search from.
+ */
+void GraphSearcher::depth_first_search(AdjacencyList *graph, int root)
+{
+    int next_vertex;
+    std::stack<int> discovered;
+    LinkedListNode *adjacent_vertex;
+
+    int *parent = new int[graph->size]{};
+    bool *visited = new bool[graph->size]{};
+
+    for (int i = 0; i < graph->size; i++)
+    {
+        parent[i] = -1;
+        visited[i] = false;
+    }
+    discovered.push(root);
+    visited[root - 1] = true;
+    parent[root - 1] = 0;
+
+    while (discovered.empty() == false)
+    {
+        next_vertex = discovered.top();
         discovered.pop();
         adjacent_vertex = graph->array[next_vertex - 1].head;
         for (int i = 0; i < graph->array[next_vertex - 1].size; i++)
