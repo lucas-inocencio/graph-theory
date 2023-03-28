9fd8d0b9161cc7f4869abd498a5ef13f1da774c9
Lucas Inocencio <lucas-inocencio@users.noreply.github.com>
2023-03-28 17:50:33 -0300

    Distance, diamater and write_parent methods.

--- src/GraphSearcher.hpp
+++ src/GraphSearcher.hpp
@@ -1,8 +1,10 @@
 #ifndef GRAPH_SEARCHER_HPP
 #define GRAPH_SEARCHER_HPP
 
+#include <fstream>
 #include <queue>
 #include <stack>
+#include <string>
 
 #include "./representations/AdjacencyList.hpp"
 
@@ -14,6 +16,9 @@ class GraphSearcher
 public:
     int *breadth_first_search(AdjacencyList *, int);
     int *depth_first_search(AdjacencyList *, int);
+    int distance(AdjacencyList *, int, int);
+    int diameter(AdjacencyList *);
+    void write_parent_file(int *, int, std::string);
 };
 
 /**
@@ -44,7 +49,7 @@ int *GraphSearcher::breadth_first_search(AdjacencyList *graph, int root)
         next_vertex = discovered.front();
         discovered.pop();
         adjacent_vertex = graph->array[next_vertex - 1].head;
-        for (int i = 0; i < graph->array[next_vertex - 1].num_vertices; i++)
+        for (int i = 0; i < graph->array[next_vertex - 1].size; i++)
         {
             if (visited[adjacent_vertex->value - 1] == false)
             {
@@ -55,6 +60,10 @@ int *GraphSearcher::breadth_first_search(AdjacencyList *graph, int root)
             adjacent_vertex = adjacent_vertex->next;
         }
     }
+
+    delete[] visited;
+
+    return parent;
 }
 
 /**
@@ -85,7 +94,7 @@ int *GraphSearcher::depth_first_search(AdjacencyList *graph, int root)
         next_vertex = discovered.top();
         discovered.pop();
         adjacent_vertex = graph->array[next_vertex - 1].head;
-        for (int i = 0; i < graph->array[next_vertex - 1].num_vertices; i++)
+        for (int i = 0; i < graph->array[next_vertex - 1].size; i++)
         {
             if (visited[adjacent_vertex->value - 1] == false)
             {
@@ -97,7 +106,54 @@ int *GraphSearcher::depth_first_search(AdjacencyList *graph, int root)
         }
     }
 
+    delete[] visited;
+
     return parent;
+}
+
+void GraphSearcher::write_parent_file(int *parent, int root, std::string file_name)
+{
+    int vertex, level;
+    int num_vertices = sizeof(parent) / sizeof(int);
+    std::ofstream outfile(file_name);
+
+    outfile << "Number of Vertices:" << num_vertices << std::endl;
+
+    for (int i = 0; i < num_vertices; i++)
+    {
+        level = 0;
+        vertex = i;
+        while (vertex != root)
+        {
+            vertex = parent[vertex - 1];
+            level++;
+        }
+
+        outfile << "Vertice:" << i+1 << "Parent:" << parent[i] << "Level:" << level << std::endl;
+    }
+
+    outfile.close();
+}
+
+int GraphSearcher::distance(AdjacencyList *graph, int root, int vertex)
+{
+    int *parent = breadth_first_search(graph, root);
+    int level = 0;
+    int child = parent[vertex - 1];
+
+    while (child != root)
+    {
+        child = parent[vertex - 1];
+        level++;
+    }
+
+    return level;
+}
+
+int GraphSearcher::diameter(AdjacencyList *graph)
+{
+    
+    
 }
 
 #endif
